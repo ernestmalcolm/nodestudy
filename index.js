a9945fe8fcd29515a318be980e3f3ee7a8b7941f
Ernest Mwinchumu <83300196+ernestmalcolm@users.noreply.github.com>
2022-03-15 12:42:08 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+
+console.log(superheroes.random());
+console.log(supervillains.random());
